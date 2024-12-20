88db428c262cb09ae7c2729f49b6bc18f3c68038
fatimatouM40 <fatimatou40275737@gmail.com>
2024-12-20 14:08:23 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SESSION HERE",
+SESSION_ID: process.env.SESSION_ID || "SVwGgSyL#91_mcrlcqOOm5Si-iuit1CLhdAAKfgC7xD9eEwciu9o",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
